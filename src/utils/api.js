768c0c288dddb91dfbaaba0c2fa4258f83b682ac
SaiSeng18 <saisengmongli14@gmail.com>
2023-02-07 18:50:13 +0630

    first-commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,12 @@
 export const API_URL = "https://api.spotify.com/v1";
 export const TOKEN_URL = "https://accounts.spotify.com/api/token";
 
+const CLIENT_ID =
+	import.meta.env.PUBLIC_SPOTIFY_CLIENT_ID || process.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET =
+	import.meta.env.PUBLIC_SPOTIFY_CLIENT_SECRET ||
+	process.env.SPOTIFY_CLIENT_SECRET;
+
 const tokenRes = await fetch(TOKEN_URL, {
 	method: "POST",
 	body: "grant_type=client_credentials",
